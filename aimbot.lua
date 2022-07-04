3843a9a0b521ba29ae0a297038c1809d91e7b3e6
Thanosdagamer <106916828+Thanosdagamer@users.noreply.github.com>
2022-07-04 13:39:20 -0400

    Create aimbot.lua

---
+++ aimbot.lua
@@ -0,0 +1,4 @@
+ _G.AimbotInput = "RightClick"
+        _G.AimbotEasing = 0.000000000000001
+        _G.TeamCheck = true
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/zeroisswag/universal-aimbot/main/script.lua"))()
